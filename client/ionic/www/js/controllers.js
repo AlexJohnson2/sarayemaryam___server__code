8714b1fbe9e46ab248973725e1a1685d929231fe
AlexJohnson2 <salambarf@gmail.com>
2021-03-04 17:48:08 +0000

    show alert for go to eitaa id

--- client/ionic/www/js/controllers.js
+++ client/ionic/www/js/controllers.js
@@ -270,12 +270,12 @@ $scope.finished_buy = function(post_data){
       $scope.showAlert = function() {
         var alertPopup = $ionicPopup.alert({
           title: 'انتقال به آیدی مسئول فروش',
-          template: '<p dir="rtl" style="line-height : 150%;font-size:23px;">سبد خرید و سفارشات شما به مسئول فروش در پیام رسان ایتا ارسال شد. برای انتقال به آیدی ایتای مسئول فروش روی دکمه زیر کلیک کنید.</p>',
-          buttons: [{text:'انتقال به آیدی مسئول فروش',type:'button-positive'}]
+          template: '<p dir="rtl" style="line-height : 150%;font-size:23px;">سبد خرید و سفارشات شما به مسئول فروش در پیام رسان ایتا ارسال شد. برای انتقال به آیدی ایتای مسئول فروش روی دکمه زیر کلیک کنید.</p><a on-click="window.open("https://eitaa.com/salambarf", "_system", "location=yes"); return false;"><button class="button button-positive">انتقال به آیدی مسئول فروش</button></a>',
+          buttons: []//{text:'انتقال به آیدی مسئول فروش',type:'button-positive'}
         });
      
         alertPopup.then(function(res) {
-          $scope.go_to_eitaa_id()
+          console.log("finish!!!")
         });
       };
       $scope.showAlert()
